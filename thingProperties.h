ac55e78c390a656052500dcd10c4a0b055546b4b
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2023-07-31 09:28:28 +0530

    Update thingProperties.h

---
+++ thingProperties.h
@@ -0,0 +1,31 @@
+#include <ArduinoIoTCloud.h>
+#include <Arduino_ConnectionHandler.h>
+
+const char DEVICE_LOGIN_NAME[]  = ""; //Device secret name
+
+const char SSID[]               = SECRET_SSID;    // Network SSID (name)
+const char PASS[]               = SECRET_OPTIONAL_PASS;    // Network password (use for WPA, or use as key for WEP)
+const char DEVICE_KEY[]  = SECRET_DEVICE_KEY;    // Secret device password
+
+void onSwitch1Change();
+void onSwitch2Change();
+void onSwitch3Change();
+void onSwitch4Change();
+
+bool switch1;
+bool switch2;
+bool switch3;
+bool switch4;
+
+void initProperties(){
+
+  ArduinoCloud.setBoardId(DEVICE_LOGIN_NAME);
+  ArduinoCloud.setSecretDeviceKey(DEVICE_KEY);
+  ArduinoCloud.addProperty(switch1, READWRITE, ON_CHANGE, onSwitch1Change, 1);
+  ArduinoCloud.addProperty(switch2, READWRITE, ON_CHANGE, onSwitch2Change);
+  ArduinoCloud.addProperty(switch3, READWRITE, ON_CHANGE, onSwitch3Change);
+  ArduinoCloud.addProperty(switch4, READWRITE, ON_CHANGE, onSwitch4Change);
+
+}
+
+WiFiConnectionHandler ArduinoIoTPreferredConnection(SSID, PASS);
